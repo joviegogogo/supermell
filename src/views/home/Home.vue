1c7dddceb376cf83d07ca966dab6c3abc9b64b0b
744461195@qq.com <744461195@qq.com>
2019-07-25 19:03:25 +0800

    第二次更新

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="centent">购物街</div></nav-bar>
+    <home-swiper :banner="banner"/>
   </div>
 </template>
 
 <script>
-import NavBar from "components/common/navbar/NavBar"
+import NavBar from "components/common/navbar/NavBar";
+import HomeSwiper from "./childComps/HomeSwiper";
+
 import {getHomeMultidata} from "network/home";
 
 
@@ -13,16 +16,20 @@ export default {
   name: "Home",
   components: {
     NavBar,
+    HomeSwiper
   },
   data(){
     return{
-
+      banner:[],  
+      recommend:[]
     }
   },
   created() {
     //1,请求多个数据
     getHomeMultidata().then(res =>{
         console.log(res);
+        this.banner = res.data.banner.list;
+        this.recommend = res.data.recommend.list;
     })
   },
 };

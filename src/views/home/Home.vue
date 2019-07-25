73f1a8ae5c5db60533ae96485383c002a71cc0b4
joviegogogo <supermanzzp@live.com>
2019-07-26 00:13:46 +0800

    第一次

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,21 +2,27 @@
   <div id="home">
     <nav-bar class="home-nav"><div slot="centent">购物街</div></nav-bar>
     <home-swiper :banner="banner"/>
+    <recommend-view :recommend="recommend" />
+    <feature-view/>
   </div>
 </template>
 
 <script>
-import NavBar from "components/common/navbar/NavBar";
-import HomeSwiper from "./childComps/HomeSwiper";
+import NavBar from "components/common/navbar/NavBar"
+import HomeSwiper from "./childComps/HomeSwiper"
+import RecommendView from "./childComps/RecommendView"
+import FeatureView from "./childComps/FeatureView"
 
-import {getHomeMultidata} from "network/home";
+import {getHomeMultidata} from "network/home"
 
 
 export default {
   name: "Home",
   components: {
     NavBar,
-    HomeSwiper
+    HomeSwiper,
+    RecommendView,
+    FeatureView
   },
   data(){
     return{
@@ -36,8 +42,17 @@ export default {
 </script>
 
 <style scoped>
+#home{
+ padding-top: 44px;  
+}
 .home-nav{
     background-color:var(--color-tint);
     color:#fff;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9; 
 }
 </style>

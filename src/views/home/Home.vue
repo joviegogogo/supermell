7ba649d6ead23ce6388c90fef709f6ac7d9c7fbf
744461195@qq.com <744461195@qq.com>
2019-07-25 15:17:06 +0800

    hehehe

---
+++ src/views/home/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="centent">购物街</div></nav-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from "components/common/navbar/NavBar"
+import {getHomeMultidata} from "network/home";
+
+
+export default {
+  name: "Home",
+  components: {
+    NavBar,
+  },
+  data(){
+    return{
+
+    }
+  },
+  created() {
+    //1,请求多个数据
+    getHomeMultidata().then(res =>{
+        console.log(res);
+    })
+  },
+};
+</script>
+
+<style scoped>
+.home-nav{
+    background-color:var(--color-tint);
+    color:#fff;
+}
+</style>
